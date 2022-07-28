b3377a44702ccdef4627f26828ec6742270b0547
vladislav <vlad.safonenko@mail.ru>
2022-07-28 21:56:21 +0300

    add swiper and he's work

---
+++ js/components/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
