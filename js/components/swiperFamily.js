73429691271c24e0828eb360b29560fce8c339fa
vladislav <vlad.safonenko@mail.ru>
2022-08-06 18:21:32 +0300

    swiper finished

--- js/components/swiperFamily.js
+++ js/components/swiperFamily.js
@@ -1,10 +1,10 @@
-new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
   pagination: {
-    el: ".swiper-pagination-2",
+    el: ".swiper-pagination",
     type: "fraction",
   },
   navigation: {
-    nextEl: ".swiper-button-next-2",
-    prevEl: ".swiper-button-prev-2",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
