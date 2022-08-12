b4dd24807d364ea376d2665b09656960ad396ec2
vladislav <vlad.safonenko@mail.ru>
2022-08-12 16:14:32 +0300

    add comments

--- js/components/comments-swiper.js
+++ js/components/comments-swiper.js
@@ -1,11 +1,7 @@
- new Swiper(".mySwiper2", {
-  slidesPerView: 3,
+new Swiper(".mySwiper2", {
   spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  navigation: {
-    nextEl: ".swiper-button-next-2",
-    prevEl: ".swiper-button-prev-2",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
