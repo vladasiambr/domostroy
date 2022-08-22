5124d26f65de05ff5bd8c73cca15c22e2e5f0c89
vladislav <vlad.safonenko@mail.ru>
2022-08-23 01:33:44 +0300

    mobile layout

---
+++ js/components/mobileSwiper.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper(".mobileSwiper", {
+  spaceBetween:30,
+  navigation: {
+    nextEl: ".swiper-button-next-3",
+    prevEl: ".swiper-button-prev-3",
+  },
+});
---
+++ js/components/swiperCommentsMobile.js
@@ -0,0 +1,13 @@
+new Swiper(".swiperCommentsMobile", {
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 15,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next6",
+    prevEl: ".swiper-button-prev6",
+  },
+});
