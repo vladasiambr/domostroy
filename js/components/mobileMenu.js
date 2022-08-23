c96ea903334b75aee035257ae00ef10e0ec88843
vlad.safonenko <vlad.safonenko@mail.ru>
2022-08-23 19:26:52 +0300

    menu active

--- js/components/mobileMenu.js
+++ js/components/mobileMenu.js
@@ -1,28 +1,28 @@
-// const humburger = document.querySelector(".hamburger");
-// const mobileMenu = document.querySelector(".header__mobile-menu");
-// const overlay = document.querySelector(".overlay");
+const humburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".header__mobile-menu");
+const exit = document.querySelector(".header__mobile-menu-exit");
 
-// humburger.addEventListener("click", openMenu);
-// overlay.addEventListener("click", openMenu);
+humburger.addEventListener("click", openMenu);
+exit.addEventListener("click", openMenu);
 
-// function openMenu() {
-//   mobileMenu.classList.toggle("header__mobile-menu--active");
-//   overlay.classList.toggle("overlay--active");
-// }
-const menu = document.querySelector(".header__mobile-menu-container");
-const menuItems = document.querySelectorAll(".header__mobile-menu-container");
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".header__mobile-menu-exit");
-const menuIcon = document.querySelector(".menuIcon");
-
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
+function openMenu() {
+  mobileMenu.classList.toggle("header__mobile-menu--active");
+  exit.classList.toggle(".header__mobile-menu-exit");
 }
+// const menu = document.querySelector(".header__mobile-menu-container");
+// const menuItems = document.querySelectorAll(".header__mobile-menu-container");
+// const hamburger = document.querySelector(".hamburger");
+// const closeIcon = document.querySelector(".header__mobile-menu-exit");
+// const menuIcon = document.querySelector(".menuIcon");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//     menuIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     menuIcon.style.display = "none";
+//   }
+// }
