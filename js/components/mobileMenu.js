b3287d8c3b4df48a22619c440ebf706ccc6f5ef1
vlad.safonenko <vlad.safonenko@mail.ru>
2022-08-23 20:53:42 +0300

    Swiper family

--- js/components/mobileMenu.js
+++ js/components/mobileMenu.js
@@ -9,20 +9,3 @@ function openMenu() {
   mobileMenu.classList.toggle("header__mobile-menu--active");
   exit.classList.toggle(".header__mobile-menu-exit");
 }
-// const menu = document.querySelector(".header__mobile-menu-container");
-// const menuItems = document.querySelectorAll(".header__mobile-menu-container");
-// const hamburger = document.querySelector(".hamburger");
-// const closeIcon = document.querySelector(".header__mobile-menu-exit");
-// const menuIcon = document.querySelector(".menuIcon");
-
-// function toggleMenu() {
-//   if (menu.classList.contains("showMenu")) {
-//     menu.classList.remove("showMenu");
-//     closeIcon.style.display = "none";
-//     menuIcon.style.display = "block";
-//   } else {
-//     menu.classList.add("showMenu");
-//     closeIcon.style.display = "block";
-//     menuIcon.style.display = "none";
-//   }
-// }
